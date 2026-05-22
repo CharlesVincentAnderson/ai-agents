7531a991622fad43aa7c2381251620a82b975c86
Chuck Anderson <charlesvincentanderson@gmail.com>
2026-05-22 11:09:06 -0400

    changed test app to fizzbuzz

--- workspace/project/tests/test_app.py
+++ workspace/project/tests/test_app.py
@@ -1,4 +1,35 @@
-from app import hello
+# test_app.py
 
-def test_hello():
-    assert hello() == "Hello world!"
+from app import fizzbuzz
+
+
+def test_fizzbuzz_1():
+    assert fizzbuzz(1) == ["1"]
+
+
+def test_fizzbuzz_3():
+    assert fizzbuzz(3) == ["1", "2", "Fizz"]
+
+
+def test_fizzbuzz_5():
+    assert fizzbuzz(5) == ["1", "2", "Fizz", "4", "Buzz"]
+
+
+def test_fizzbuzz_15():
+    assert fizzbuzz(15) == [
+        "1",
+        "2",
+        "Fizz",
+        "4",
+        "Buzz",
+        "Fizz",
+        "7",
+        "8",
+        "Fizz",
+        "Buzz",
+        "11",
+        "Fizz",
+        "13",
+        "14",
+        "FizzBuzz",
+    ]
